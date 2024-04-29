075b18f449f2d7e368dc4927f6d141ffe589a017
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-04-29 08:43:08 -0300

    add OrderStatus enum

--- demo/src/main/java/com/joaolima/demo/config/TestConfig.java
+++ demo/src/main/java/com/joaolima/demo/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.joaolima.demo.config;
 
 import com.joaolima.demo.entities.Order;
 import com.joaolima.demo.entities.User;
+import com.joaolima.demo.entities.com.joaolima.demo.entities.enums.OrderStatus;
 import com.joaolima.demo.repositories.OrderRepository;
 import com.joaolima.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig  implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "98881231", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "981231241", "123452");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
