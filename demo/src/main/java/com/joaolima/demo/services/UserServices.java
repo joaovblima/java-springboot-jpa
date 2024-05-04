acb3effa9afdd3162d3286309420584a4b0d8cec
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-05-04 14:09:52 -0300

    User delete

--- demo/src/main/java/com/joaolima/demo/services/UserServices.java
+++ demo/src/main/java/com/joaolima/demo/services/UserServices.java
@@ -26,4 +26,7 @@ public class UserServices {
     public User insert(User obj) {
         return repository.save(obj);
     }
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
