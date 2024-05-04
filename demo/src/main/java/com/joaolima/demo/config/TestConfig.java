a373730c9505d58093d411edaabad8aea8150fe5
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-05-04 11:07:30 -0300

    payment one-to-one association

--- demo/src/main/java/com/joaolima/demo/config/TestConfig.java
+++ demo/src/main/java/com/joaolima/demo/config/TestConfig.java
@@ -1,10 +1,6 @@
 package com.joaolima.demo.config;
 
-import com.joaolima.demo.entities.Category;
-import com.joaolima.demo.entities.Order;
-import com.joaolima.demo.entities.User;
-import com.joaolima.demo.entities.Product;
-import com.joaolima.demo.entities.OrderItem;
+import com.joaolima.demo.entities.*;
 import com.joaolima.demo.entities.com.joaolima.demo.entities.enums.OrderStatus;
 import com.joaolima.demo.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -79,6 +75,11 @@ public class TestConfig  implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
 
     }
